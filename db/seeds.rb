b0cfc136074396a7cf00a5209b85205c6f4a611f
wjorellano <w.orellano@opensky.com.co>
2024-06-13 15:56:14 -0500

    se arreglo el diseño del admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,59 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+Faker::Config.locale = 'es'
+
+# Crear Categorías
+10.times do
+  Category.create!(
+    name: Faker::Commerce.department,
+    description: Faker::Lorem.sentence
+  )
+end
+
+# Crear Productos
+50.times do
+  Product.create!(
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.paragraph,
+    price: Faker::Commerce.price(range: 100..1000),
+    category_id: Category.pluck(:id).sample,
+    active: [true, false].sample
+  )
+end
+
+# Crear Órdenes
+20.times do
+  Order.create!(
+    customer_email: Faker::Internet.email,
+    fulfilled: [true, false].sample,
+    total: Faker::Commerce.price(range: 100..5000),
+    address: Faker::Address.full_address
+  )
+end
+
+# Crear OrderProducts
+100.times do
+  OrderProduct.create!(
+    product_id: Product.pluck(:id).sample,
+    order_id: Order.pluck(:id).sample,
+    size: ['S', 'M', 'L', 'XL'].sample,
+    quantity: Faker::Number.between(from: 1, to: 10)
+  )
+end
+
+# Crear Stocks
+Product.all.each do |product|
+  ['S', 'M', 'L', 'XL'].each do |size|
+    Stock.create!(
+      size: size,
+      amount: Faker::Number.between(from: 10, to: 100),
+      product_id: product.id
+    )
+  end
+end
+
+p "Seed creado"
